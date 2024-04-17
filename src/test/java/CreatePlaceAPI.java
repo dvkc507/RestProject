0c7d6a9ef2ec63f6dec03d379c86cff8e201094e
Venkata Krishna Chaitanya Doppalapudi <VDoppalapudi@prolifics.com>
2024-04-17 14:00:41 +0530

    Adding API code

---
+++ src/test/java/CreatePlaceAPI.java
@@ -0,0 +1,36 @@
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class CreatePlaceAPI {
+	
+	@Test
+	public void createPlace() {
+		//body method accept/require JSON string fromat
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		Response response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body("{\r\n"
+				+ "  \"location\": {\r\n"
+				+ "    \"lat\": -38.383494,\r\n"
+				+ "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n"
+				+ "  \"accuracy\": 50,\r\n"
+				+ "  \"name\": \"Frontline house\",\r\n"
+				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+				+ "  \"types\": [\r\n"
+				+ "    \"shoe park\",\r\n"
+				+ "    \"shop\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"website\": \"http://google.com\",\r\n"
+				+ "  \"language\": \"French-IN\"\r\n"
+				+ "}\r\n"
+				+ "").when().post("/maps/api/place/add/json")
+		.then().log().all().assertThat().statusCode(200).header("Content-Type", "application/json;charset=UTF-8").extract().response();
+		
+	}
+
+}
